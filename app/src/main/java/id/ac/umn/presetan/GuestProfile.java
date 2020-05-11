4ab704b303d9ad542ac65d70a3d2b1e0de8eb0a7
r00sha <r00sha@users.noreply.github.com>
2020-05-11 16:55:02 +0700

    Function back dari guest profile

--- app/src/main/java/id/ac/umn/presetan/GuestProfile.java
+++ app/src/main/java/id/ac/umn/presetan/GuestProfile.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -35,5 +36,14 @@ public class GuestProfile extends AppCompatActivity {
                 startActivity(signIn);
             }
         });
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
